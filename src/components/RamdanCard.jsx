5ca53e89f978982d803120776a242b541ebf16f7
Zer0-Dark <60540945+Zer0-Dark@users.noreply.github.com>
2025-02-14 19:59:34 +0200

    fixing the loading

--- src/components/RamdanCard.jsx
+++ src/components/RamdanCard.jsx
@@ -2,8 +2,8 @@ import ramdan from "../assets/ramdan2.svg"
 
 function RamdanCard() {
     return (
-        <div className=" fixed bottom-2 right-2 pb-4   bg-mainBg border-2 border-black rounded-md z-50  flex justify-center items-center flex-col">
-            <img className="w-44" src={ramdan} alt=">رمضان كريم"></img>
+        <div className=" fixed bottom-12 right-2 pb-4   bg-mainBg  rounded-md z-20  flex justify-center items-center flex-col">
+            <img className="w-52" src={ramdan} alt=">رمضان كريم"></img>
             <h2 className="-mt-4 text-xl font-mainFont ">كل عام و انتم بخير</h2>
         </div>
     )
