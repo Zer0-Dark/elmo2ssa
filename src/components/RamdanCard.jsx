4e52c6b21ac6593b6e1b03d45016fa351257e3b5
Zer0-Dark <60540945+Zer0-Dark@users.noreply.github.com>
2025-02-14 20:13:06 +0200

    fix the lanturn

--- src/components/RamdanCard.jsx
+++ src/components/RamdanCard.jsx
@@ -2,7 +2,7 @@ import ramdan from "../assets/ramdan2.svg"
 
 function RamdanCard() {
     return (
-        <div className=" fixed bottom-12 right-2 pb-4   bg-mainBg  rounded-md z-20  flex justify-center items-center flex-col">
+        <div className=" fixed desktop:flex hidden  bottom-12 right-2 pb-4   bg-mainBg  rounded-md z-20   justify-center items-center flex-col">
             <img className="w-52" src={ramdan} alt=">رمضان كريم"></img>
             <h2 className="-mt-4 text-xl font-mainFont ">كل عام و انتم بخير</h2>
         </div>
